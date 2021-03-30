c7ec18a1ecb4e25bd727e6a10af44c419fc216cb
Nick Matté <59057331+NickMatte@users.noreply.github.com>
2021-03-30 00:50:55 -0300

    Atividade 2

---
+++ app/src/main/java/com/example/nossaprimeiraappnoite/DetalheTodoActivity.java
@@ -0,0 +1,59 @@
+package com.example.nossaprimeiraappnoite;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.nossaprimeiraappnoite.Model.Todo;
+
+
+public class DetalheTodoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhe_todo);
+        Intent intent = getIntent();
+        Parcelable prc = intent.getParcelableExtra("objTodo");
+        Todo todo = intent.getParcelableExtra("objTodo");
+        TextView tv = findViewById(R.id.tvId);
+        tv.setText(todo.getId()+"");
+        tv = findViewById(R.id.tvIdUser);
+        tv.setText(todo.getUserId()+"");
+        tv = findViewById(R.id.tvTitle);
+        tv.setText(todo.getTitle());
+        CheckBox cb = findViewById(R.id.cbCompleted);
+        cb.setChecked(todo.isCompleted());
+        bind(todo);
+
+    }
+    public void trocaLayout(View v) {
+        setContentView(R.layout.layout);
+        Intent intent = getIntent();
+        Todo todo = intent.getParcelableExtra("objTodo");
+        bind(todo);
+
+    }
+    public void cbClick(View v) {
+        CheckBox cb = findViewById(R.id.cbCompleted);
+        Intent intent = getIntent();
+        Todo todo = intent.getParcelableExtra("objTodo");
+        todo.setCompleted(cb.isChecked());
+    }
+    private void bind(Todo obj) {
+        TextView tv = findViewById(R.id.tvId);
+        tv.setText(obj.getId()+"");
+        tv = findViewById(R.id.tvIdUser);
+        tv.setText(obj.getUserId()+"");
+        tv = findViewById(R.id.tvTitle);
+        tv.setText(obj.getTitle());
+        CheckBox cb = findViewById(R.id.cbCompleted);
+        cb.setChecked(obj.isCompleted());
+
+    }
+}
